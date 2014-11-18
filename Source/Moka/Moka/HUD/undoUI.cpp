3f895074195f86616a4f66d57faed375a5f91526
Aaron Whitesell <axwhitesell@gmail.com>
2014-11-18 17:24:17 +0300

    Added an UndoUI class.

---
+++ Source/Moka/Moka/HUD/undoUI.cpp
@@ -0,0 +1,129 @@
+#include "undoUI.h"
+#include "../Resources/resourceIdentifiers.h"
+
+#include <SFML/Graphics/RenderStates.hpp>
+#include <SFML/Graphics/RenderTarget.hpp>
+
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+
+
+UndoUI::UndoUI(Fonts::ID font, trmb::FontHolder &fonts, SoundEffects::ID soundEffect, trmb::SoundPlayer &soundPlayer,
+	EventGuid leftClickPress, EventGuid leftClickRelease)
+: mLeftClickPress(leftClickPress)
+, mLeftClickRelease(leftClickRelease)
+, mFont(font)
+, mFonts(fonts)
+, mSoundEffect(soundEffect)
+, mSoundPlayer(soundPlayer)
+, mUIElemSize(sf::Vector2f(50.0f, 20.0f))
+, mOutLineThickness(1.0f)
+, mFrameBuffer(sf::Vector2f(7.0f, 7.0f))
+, mHorizontalBuffer(1.0f)
+{
+	mBackground.setPosition(0.0f, 0.0f);
+	mBackground.setFillColor(sf::Color(0u, 0u, 0u, 200u));
+	mBackground.setOutlineColor(sf::Color(0u, 0u, 0u, 255u));
+	mBackground.setOutlineThickness(mOutLineThickness);
+	// ALW - Default mBackground size as if there is one UI element, so it is visible by default.
+	mBackground.setSize(sf::Vector2f(mFrameBuffer.x * 2.0f + mUIElemSize.x, mFrameBuffer.y * 2.0f + mUIElemSize.y));
+}
+
+sf::Vector2f UndoUI::getSize() const
+{
+	return mBackground.getSize();
+}
+
+sf::FloatRect UndoUI::getRect() const
+{
+	const sf::Vector2f backgroundPosition = mBackground.getPosition();
+	const sf::Vector2f backgroundSize = mBackground.getSize();
+	sf::FloatRect rect = sf::FloatRect(backgroundPosition.x, backgroundPosition.y, backgroundSize.x, backgroundSize.y);
+	rect = getTransform().transformRect(rect);
+
+	return rect;
+}
+
+void UndoUI::setSize(sf::Vector2f size)
+{
+	for (const auto& uiElem : mUIElems)
+	{
+		uiElem->setSize(size);
+	}
+
+	buildUI();
+}
+
+void UndoUI::handler(const sf::RenderWindow &window, const sf::View &view, const sf::Transform &transform)
+{
+	sf::Transform combinedTransform = getTransform() * transform;
+
+	for (const auto &uiElem : mUIElems)
+	{
+		uiElem->handler(window, view, combinedTransform);
+	}
+}
+
+void UndoUI::draw(sf::RenderTarget &target, sf::RenderStates states) const
+{
+	states.transform *= getTransform();
+
+	target.draw(mBackground, states);
+
+	for (const auto &uiElem : mUIElems)
+	{
+		target.draw(*uiElem, states);
+	}
+}
+
+void UndoUI::addUIElem(std::string doString, std::string undoString, Callback doCallback, Callback undoCallback)
+{
+	mUIElems.emplace_back(std::unique_ptr<trmb::UndoUIElem>(new trmb::UndoUIElem(mFont, mFonts, mSoundEffect, mSoundPlayer
+		, mLeftClickPress, mLeftClickRelease)));
+	mUIElems.back()->setText(doString, undoString);
+	mUIElems.back()->setVisualScheme(sf::Color(95u, 158u, 160u, 255u), sf::Color(255u, 255u, 255u, 255u), sf::Color(0u, 0u, 0u, 255u)
+		, sf::Color(162u, 181u, 205u, 255u), sf::Color(255u, 255u, 255u, 255u), sf::Color(0u, 0u, 0u, 255u)       // Hover
+		, sf::Color(88u, 146u, 148u, 255u), sf::Color(255u, 255u, 255u, 255u), sf::Color(0u, 0u, 0u, 255u)        // Depress
+		, sf::Color(160u, 160u, 160u, 255u), sf::Color(224u, 224u, 224u, 255u), sf::Color(128u, 128u, 128u, 255u) //Disablle
+		, mOutLineThickness);
+
+	buildUI();
+}
+
+void UndoUI::buildUI()
+{
+	if (!mUIElems.empty())
+	{
+		sf::Vector2f uiElemOffset = mFrameBuffer;
+		sf::Vector2f backgroundSize;
+
+		for (const auto& uiElems : mUIElems)
+		{
+			uiElems->setPosition(uiElemOffset);
+			uiElemOffset.x = uiElems->getPosition().x + uiElems->getSize().x + mHorizontalBuffer;
+
+			backgroundSize.x += uiElems->getSize().x;
+			backgroundSize.y = std::max(backgroundSize.y, uiElems->getSize().y);
+		}
+
+		backgroundSize.x = backgroundSize.x + mFrameBuffer.x * 2.0f + mHorizontalBuffer * (mUIElems.size() - 1);
+		backgroundSize.y = backgroundSize.y + mFrameBuffer.y * 2.0f;
+		mBackground.setSize(backgroundSize);
+	}
+}
+
+void centerOrigin(UndoUI &undoUI, bool centerXAxis, bool centerYAxis)
+{
+	sf::Vector2f size = undoUI.getSize();
+	float xAxis = 0.0f;
+	float yAxis = 0.0f;
+
+	if (centerXAxis)
+		xAxis = std::floor(size.x / 2.0f);
+
+	if (centerYAxis)
+		yAxis = std::floor(size.y / 2.0f);
+
+	undoUI.setOrigin(xAxis, yAxis);
+}
---
+++ Source/Moka/Moka/HUD/undoUI.h
@@ -0,0 +1,81 @@
+#ifndef UNDO_UI_H
+#define UNDO_UI_H
+
+#include "Trambo/HUD/undoUIElem.h"
+#include "Trambo/Resources/resourceHolder.h"
+
+#include <SFML/Graphics/Drawable.hpp>
+#include <SFML/Graphics/Transformable.hpp>
+#include <SFML/Graphics/Rect.hpp>
+#include <SFML/System/Vector2.hpp>
+
+#include <memory>
+#include <vector>
+
+
+namespace sf
+{
+	class RenderStates;
+	class RenderTarget;
+	class RenderWindow;
+	class Transform;
+	class View;
+}
+
+namespace trmb
+{
+	class SoundPlayer;
+}
+
+class UndoUI : public sf::Transformable, public sf::Drawable
+{
+public:
+	using Callback = trmb::UndoUIElem::Callback;
+	using EventGuid = unsigned long;
+
+
+public:
+							UndoUI(Fonts::ID font, trmb::FontHolder &fonts, SoundEffects::ID soundEffect
+								, trmb::SoundPlayer &soundPlayer, EventGuid leftClickPress, EventGuid leftClickRelease);
+							UndoUI(const UndoUI &) = delete;
+	UndoUI&					operator=(const UndoUI &) = delete;
+
+	sf::Vector2f			getSize() const;
+	sf::FloatRect			getRect() const;
+
+	void					setSize(sf::Vector2f size);
+
+	void					handler(const sf::RenderWindow &window, const sf::View &view, const sf::Transform &transform);
+	void					addUIElem(std::string doString, std::string undoString, Callback doCallback, Callback undoCallback);
+
+
+private:
+	using UIElemPtr = std::unique_ptr<trmb::UndoUIElem>;
+	using UIElemVec = std::vector<UIElemPtr>;
+
+
+private:
+	virtual void			draw(sf::RenderTarget &target, sf::RenderStates states) const override final;
+	void					buildUI();
+
+
+private:
+	const EventGuid			mLeftClickPress;	// ALW - Matches the GUID in the Controller class.
+	const EventGuid			mLeftClickRelease;	// ALW - Matches the GUID in the Controller class.
+	Fonts::ID				mFont;
+	trmb::FontHolder&		mFonts;
+	SoundEffects::ID		mSoundEffect;
+	trmb::SoundPlayer&		mSoundPlayer;
+
+	sf::Vector2f			mUIElemSize;
+	const float				mOutLineThickness;
+	const sf::Vector2f		mFrameBuffer;
+	float					mHorizontalBuffer;
+
+	UIElemVec				mUIElems;
+	sf::RectangleShape		mBackground;
+};
+
+void	centerOrigin(UndoUI &undoUI, bool centerXAxis = true, bool centerYAxis = true);
+
+#endif
